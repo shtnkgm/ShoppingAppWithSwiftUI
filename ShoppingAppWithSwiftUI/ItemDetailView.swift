360eebe808cecb18596d80d88c68d4c834adf635
snakagam <snakagam@yahoo-corp.jp>
2019-06-09 10:07:11 -0700

    fix

--- ShoppingAppWithSwiftUI/ItemDetailView.swift
+++ ShoppingAppWithSwiftUI/ItemDetailView.swift
@@ -16,7 +16,7 @@ struct ItemDetailView : View {
             Image(item.imageName)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-            Group {
+            VStack(alignment: .leading, spacing: 8) {
                 Text(item.name)
                     .font(.headline)
                     .lineLimit(nil)
@@ -26,7 +26,7 @@ struct ItemDetailView : View {
                 Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
                     .font(.body)
                     .lineLimit(nil)
-                }.padding(.horizontal, 20)
+                }.padding(.horizontal, 16)
         }
     }
 }
