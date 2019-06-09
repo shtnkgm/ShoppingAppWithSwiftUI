f8afe55c6b7abd5a78573e9262198634aaa25b70
snakagam <snakagam@yahoo-corp.jp>
2019-06-08 21:28:02 -0700

    fix

--- ShoppingAppWithSwiftUI/ItemDetailView.swift
+++ ShoppingAppWithSwiftUI/ItemDetailView.swift
@@ -17,15 +17,16 @@ struct ItemDetailView : View {
                 .resizable()
                 .aspectRatio(contentMode: .fit)
             Group {
-            Text(item.name)
-                .font(.headline)
-            Text("\(item.price)円")
-                .font(.subheadline)
-                .color(Color.gray)
-            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-                .font(.body)
-                .lineLimit(nil)
-            }
+                Text(item.name)
+                    .font(.headline)
+                    .lineLimit(nil)
+                Text("\(item.price)円")
+                    .font(.subheadline)
+                    .color(Color.gray)
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+                    .font(.body)
+                    .lineLimit(nil)
+                }.padding(.horizontal, 20)
         }
     }
 }
